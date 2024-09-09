937558e4d1cfc7ce7fa3014e489fed094bdbdc52
Truong Phan <phanlethanhtruong@gmail.com>
2024-09-09 21:11:57 +0700

    add:falie

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: "/start-recipe_page/",
+  plugins: [react()],
+  base: "/start-recipe_page/"
 })
